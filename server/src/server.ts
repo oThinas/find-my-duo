b8b267bd62b548bcf3f4dfc9962ef34e33516204
oThinas <thiagomartinsprado@gmail.com>
2022-10-22 18:58:03 -0300

    Revert "feat: deploy do server"
    This reverts commit bd0dc17c68cf548d41196a04e8400597bdd275bb.

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,11 @@ import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-stri
 
 const app = express()
 app.use(express.json())
-app.use(cors()) /* Aberto a todos */
+app.use(cors()) /* Por enquanto, aberto a todos */
+/* TODO: Antes de subir para produção, especificar o domínio que poderá acessar esse backend */
+/* app.use(cors({
+  origin: 'dominio'
+})) */
 
 const prisma = new PrismaClient()
 
@@ -89,6 +93,8 @@ app.get('/ads/:id/discord', async (request, response) => {
 app.post('/games/:id/ads', async (request, response) => {
   const gameId = request.params.id
   const body = request.body
+
+  /* TODO: Validação com a biblioteca Zod */
 
   const ad = await prisma.ad.create({
     data: {
